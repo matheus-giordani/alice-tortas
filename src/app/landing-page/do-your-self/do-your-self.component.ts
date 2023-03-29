2df06ad7fd71c2c17738e857013978bd26c56b0e
matheus-giordani <matheus.giordanioliveira@gmail.com>
2023-03-29 20:15:12 -0300

    add campo dinamico de adicionais

--- src/app/landing-page/do-your-self/do-your-self.component.ts
+++ src/app/landing-page/do-your-self/do-your-self.component.ts
@@ -8,63 +8,81 @@ import { Console } from 'console';
   selector: 'app-do-your-self',
   templateUrl: './do-your-self.component.html',
   styleUrls: ['./do-your-self.component.css'],
-  providers: [DatePipe]
+  providers: [DatePipe],
 })
 export class DoYourSelfComponent implements OnInit {
   phoneNumber = '558192524545'; // número de telefone do destinatário, incluindo o código do país
   message: string | undefined; // mensagem que será enviada
   baseUrl = 'https://api.whatsapp.com/send';
-  link: any
-  constructor(private formBuilder: FormBuilder,private router: Router, private datePipe: DatePipe ) {}
+  link: any;
+  valor: string | undefined;
+  condicao = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private datePipe: DatePipe
+  ) {}
 
-  public form : FormGroup = this.formBuilder.group({
+  public form: FormGroup = this.formBuilder.group({
     massa: [null, Validators.required],
     recheio: [null, Validators.required],
+    adicionais: [null],
     quantidade: [null, Validators.required],
-    comentario: [null]
-  })
+    comentario: [null],
+  });
 
   ngOnInit(): void {
-
-
+    this.form.valueChanges.subscribe((formValues) => {
+      if (formValues.massa === 'Salgada' && formValues.recheio === 'Frango') {
+        this.condicao = true;
+      }
+    });
   }
 
-  submit(){
-    console.log("teste")
-    console.log(this.form.value)
-    this.message =
-
-    `
+  submit() {
+    if (this.condicao) {
+      this.message = `
     \t \ud83e\uddc1*INFORMAÇÕES DO PEDIDO*\ud83e\uddc1
 
-    \ud83c\udf6a *Massa:* ${(this.form.value).massa}
-    \ud83c\udf6a *Recheio:* ${(this.form.value).recheio}
-    \ud83c\udf6a *Quantidade:* ${(this.form.value).quantidade}
-    \ud83c\udf6a *Data do pedido:* ${this.getDate() }
-    \ud83c\udf6a *Comentario:* ${((this.form.value).comentario) || ''}
+    \ud83c\udf6a *Massa:* ${this.form.value.massa}
+    \ud83c\udf6a *Recheio:* ${this.form.value.recheio}
+    \ud83c\udf6a *Adicionais:* ${this.form.value.adicionais}
+    \ud83c\udf6a *Quantidade:* ${this.form.value.quantidade}
+    \ud83c\udf6a *Data do pedido:* ${this.getDate()}
+    \ud83c\udf6a *Comentario:* ${this.form.value.comentario || ''}
 
-    `
-    this.link = this.constructorRoute(this.message)
-    window.location.href = this.link
+    `;
+    } else {
+      this.message = `
+    \t \ud83e\uddc1*INFORMAÇÕES DO PEDIDO*\ud83e\uddc1
 
+    \ud83c\udf6a *Massa:* ${this.form.value.massa}
+    \ud83c\udf6a *Recheio:* ${this.form.value.recheio}
+    \ud83c\udf6a *Quantidade:* ${this.form.value.quantidade}
+    \ud83c\udf6a *Data do pedido:* ${this.getDate()}
+    \ud83c\udf6a *Comentario:* ${this.form.value.comentario || ''}
 
+    `;
+    }
+    this.link = this.constructorRoute(this.message);
+    window.location.href = this.link;
   }
 
-  getDate(){
-    const currentDate = new Date()
-    const dateFormated = this.datePipe.transform(currentDate, 'dd/MM/yyyy HH:mm:ss')
-    return dateFormated
-
+  getDate() {
+    const currentDate = new Date();
+    const dateFormated = this.datePipe.transform(
+      currentDate,
+      'dd/MM/yyyy HH:mm:ss'
+    );
+    return dateFormated;
   }
 
+  onChange(event: any) {}
 
-
-  constructorRoute(message: any){
+  constructorRoute(message: any) {
     const queryString = `phone=${encodeURIComponent(
       this.phoneNumber
     )}&text=${encodeURIComponent(message)}`;
     return `${this.baseUrl}?${queryString}`;
-
-
   }
 }
